7679f1a303bebf02052412a214dbd1b26125f107
Nikoaly Trifonov <koksi75@gmail.com>
2016-09-23 22:03:35 +0300

    Add project files.

---
+++ Exact_Sum_of_Real_Numbers/Exact_Sum_of_Real_Numbers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exact_Sum_of_Real_Numbers
+{
+    class Exact_Sum_of_Real_Numbers
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < numbers; i++)
+            {
+                decimal num = decimal.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Refactor_Special_Numbers/Refactor_Special_Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactor_Special_Numbers
+{
+    class Refactor_Special_Numbers
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            for (int num = 1; num <= numbers; num++)
+            {
+                int sum = 0;
+                int number = num;
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number = number / 10;
+                }
+                bool specialNumbers = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", num, specialNumbers);
+            }
+        }
+    }
+}
